499cc0c49af269895144edbf90f01530189b7752
edr3x <dhunganaannanta@gmail.com>
2023-03-17 12:40:54 +0545

    form improved

--- src/Demo_10_Form/FormDemo.tsx
+++ src/Demo_10_Form/FormDemo.tsx
@@ -6,7 +6,7 @@ export function FormDemo() {
     const schema = object({
         fullName: z.string().min(3).max(20),
         email: z.string().email(),
-        age: z.number().min(18),
+        age: z.number().min(18).max(60),
         passowrd: z.string().min(6).max(20),
         confirmPassword: z.string().min(6).max(20),
     }).refine((data) => data.passowrd === data.confirmPassword, {
@@ -32,36 +32,52 @@ export function FormDemo() {
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <input
+                    className={
+                        errors.fullName?.message ? "inputError" : "inputBox"
+                    }
                     type="text"
                     placeholder="Full Name..."
                     {...register("fullName")}
                 />
                 <p>{errors.fullName?.message}</p>
                 <input
+                    className={
+                        errors.email?.message ? "inputError" : "inputBox"
+                    }
                     type="text"
                     placeholder="Email..."
                     {...register("email")}
                 />
                 <p>{errors.email?.message}</p>
                 <input
+                    className={errors.age?.message ? "inputError" : "inputBox"}
                     type="number"
+                    pattern="[0-9]*"
                     placeholder="Age..."
-                    {...register("age")}
+                    {...register("age", { valueAsNumber: true })}
                 />
                 <p>{errors.age?.message}</p>
                 <input
+                    className={
+                        errors.passowrd?.message ? "inputError" : "inputBox"
+                    }
                     type="password"
                     placeholder="passowrd..."
                     {...register("passowrd")}
                 />
                 <p>{errors.passowrd?.message}</p>
                 <input
+                    className={
+                        errors.confirmPassword?.message
+                            ? "inputError"
+                            : "inputBox"
+                    }
                     type="password"
                     placeholder="Confirm Password..."
                     {...register("confirmPassword")}
                 />
                 <p>{errors.confirmPassword?.message}</p>
-                <input type="submit" value="Submit" />
+                <input className="submitButton" type="submit" value="Submit" />
             </form>
         </div>
     );
